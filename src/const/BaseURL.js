a2d0736575d989be9514650a4dd25a7d144c8ab3
Le Hoang Loi <nhoxac94@yahoo.com>
2022-08-24 15:51:59 +0700

    fix router

---
+++ src/const/BaseURL.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://booking-app-demo.herokuapp.com/api'
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,16 +1,19 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { BASE_URL } from '../const/BaseURL'
 
 const useFetch = (url) => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
-
+    const path = BASE_URL + url
+    console.log(BASE_URL)
+    console.log(url)
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true)
             try {
-                const res = await axios.get(url)
+                const res = await axios.get(path)
                 setData(res.data)
             } catch (err) {
                 setError(err)
@@ -18,12 +21,12 @@ const useFetch = (url) => {
             setLoading(false)
         }
         fetchData()
-    }, [url])
+    }, [path])
 
     const reFetch = async () => {
         setLoading(true)
         try {
-            const res = await axios.get(url)
+            const res = await axios.get(path)
             setData(res.data)
         } catch (err) {
             setError(err)
